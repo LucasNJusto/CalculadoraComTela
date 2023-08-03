a5c1649a7444ce0967081dde1dd77596f798e956
LUCAS NOVAES JUSTO <lucas.njusto@senacsp.edu.br>
2023-08-03 16:30:40 -0300

    Primeiro Commit

---
+++ CalculadoraForms/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraForms
+{
+    public partial class Form1 : Form
+    {
+        ModelCalculadora modelo;
+        //Conectando a Control com a modelo
+        public Form1()
+        {
+            InitializeComponent();
+            this.modelo = new ModelCalculadora();
+        }
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = "" + this.modelo.Divisao(Convert.ToDouble(maskedTextBox1.Text));
+        }//Fim da Divisao
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = "" + this.modelo.Somar(Convert.ToDouble(maskedTextBox1.Text));
+        }//Fim da soma
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = "" + this.modelo.Subtrair(Convert.ToDouble(maskedTextBox1.Text));
+        }//Fim da subtração
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = "" + this.modelo.Mutiplicacao(Convert.ToDouble(maskedTextBox1.Text));
+        }//Fim da Mutiplicação
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = "" + this.modelo.clear(Convert.ToDouble(maskedTextBox1.Text));
+        }//Fim do Resultado
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }// Onde vai digitar
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = "" + this.modelo.Raiz(Convert.ToDouble(maskedTextBox1.Text));
+        }//Raiz
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = "" + this.modelo.Potencia(Convert.ToDouble(maskedTextBox1.Text));
+        }//Potencia
+    }//Fim da classe
+}//Fim do Projeto
